9818905fef6201c0e6aeed1b62274c67909213ae
Edson Araujo <edsonaraujodesouzaneto@gmail.com>
2023-10-07 17:23:59 -0300

    Update Principal.java

--- simulacao-de-trens/Principal.java
+++ simulacao-de-trens/Principal.java
@@ -14,6 +14,7 @@ import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.image.Image;
+import controller.PlataformaController;
 
 public class Principal extends Application {
 
